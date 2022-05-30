a94e386f1330ba35efd0eb5a63ce90525e14b586
TheAmityD <88772700+TheAmityD@users.noreply.github.com>
2022-05-30 21:34:32 +0800

    Touches

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("127.0.0.1","root","");
+$con=mysqli_connect("localhost","root","");
 mysqli_select_db($con,"kcdb");
 ?>
 <!DOCTYPE html>
@@ -68,6 +68,32 @@ mysqli_select_db($con,"kcdb");
             -ms-transition: background-color 100ms linear;
         }
 
+        .file-input::-webkit-file-upload-button {
+            visibility: hidden;
+            display: none;
+        }
+        .file-input::before {
+            content: 'Select image';
+            display: inline-block;
+            background: linear-gradient(top, #f9f9f9, #e3e3e3);
+            border: 1px solid #aba695;
+            border-radius: 3px;
+            padding: 5px 8px;
+            outline: none;
+            white-space: nowrap;
+            -webkit-user-select: none;
+            cursor: pointer;
+            text-shadow: 1px 1px #fff;
+            font-weight: 700;
+            font-size: 10pt;
+            margin-right: 15px;
+        }
+        .file-input:hover::before {
+            border-color: #c29bb3;
+        }
+        .file-input:active::before {
+            background: #f0d5e6;
+        }
     </style>
 </head>
 <body>
@@ -85,7 +111,7 @@ mysqli_select_db($con,"kcdb");
     
     <div class="form">
     <h1>Add members information</h1>
-    <form method="POST" action="insert.php">
+    <form method="POST" action="insert.php" enctype="multipart/form-data">
         <label for="name">Name:</label><br>
         <input type="text" name="name"></input><br><br>
 
@@ -99,31 +125,57 @@ mysqli_select_db($con,"kcdb");
         <input type="text" name="phone" maxlength="12"></input><br><br>
         
         <label for="image">Image link:</label><br>
-        <input type="text" name="image"></input><br><br>
+        <input type="file" name="fileToUpload" id="fileToUpload" class="file-input"></input><br><br>
 
         <input type="button" value="Back" onClick="location.href='about-us-admin.php'"></input>
         <input type="submit" value="Submit!" name="insertdata"></input>
         <?php 
-        if(isset($_POST["insertdata"]))
-        {
-            $name=$_POST["name"];
-            $age=$_POST["age"];
-            $email=$_POST["email"];
-            $phone=$_POST["phone"];
-            $image=$_POST["image"];
+        if(isset($_POST["insertdata"])) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+              $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+              if($check !== false) {
+                $uploadOk = 1;
+              } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+              }
             
-            $query="insert into info values ('','$name','$age','$email','$phone','$image')";
-            $query_run=mysqli_query($con,$query);
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+              echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+              $uploadOk = 0;
+            }
             
-            if($query_run)
-            {
-                echo "<script type='text/javascript'>alert ('Info added!')</script>";
-                echo"<script type=text/javascript>window.location.replace('about-us-admin.php')</script>";
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+              echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    $name=$_POST["name"];
+                    $age=$_POST["age"];
+                    $email=$_POST["email"];
+                    $phone=$_POST["phone"];
+                    
+                    $query="insert into info values ('','$name','$age','$email','$phone','$target_file')";
+                    $query_run=mysqli_query($con,$query);
+                    
+                    if($query_run)
+                    {
+                        echo "<script type='text/javascript'>alert ('Info added!')</script>";
+                        echo"<script type=text/javascript>window.location.replace('about-us-admin.php')</script>";
+                    }
+                    else
+                    {
+                        echo "<script type='text/javascript'>alert ('Whoops something went wrong')</script>";
+                    }
+                }
             }
-            else
-            {
-                echo "<script type='text/javascript'>alert ('Whoops something went wrong')</script>";
-            }
+            
         }
         ?>
     </form>
